44a512be2fd387912418a17a483ed4d63a27dcbd
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-01-17 00:24:45 +0900

    Add new project

---
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -0,0 +1,75 @@
+package com.example.demo.login.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.login.domain.model.GroupOrder;
+import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.UserService;
+
+@Controller
+public class SignupController {
+
+	@Autowired
+	private UserService userService;
+	
+	private Map<String, String> radioMarriage;	
+	private Map<String, String> initRadioMarriage(){
+		
+		Map<String, String> radio = new LinkedHashMap<>();
+		
+		//既婚、未婚をMapに格納
+		radio.put("既婚","true");
+		radio.put("未婚","false");
+
+		return radio;
+	}
+	
+	@GetMapping("/signup")
+	public String getSignUp(@ModelAttribute SignupForm form, Model model) {
+		radioMarriage = initRadioMarriage();
+		model.addAttribute("radioMarriage",radioMarriage);
+
+		return "login/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model) {
+		
+		if (bindingResult.hasErrors()) {
+			return getSignUp(form, model);
+		}
+		System.out.println(form);
+		
+		//insert用変数
+		User user = new User();
+		user.setUserId(form.getUserId());//ユーザーID
+		user.setPassword(form.getPassword());//パスワード
+		user.setUserName(form.getUserName());//ユーザー名
+		user.setBirthday(form.getBirthday());//誕生日
+		user.setAge(form.getAge());//年齢
+		user.setMarriage(form.isMarriage());//結婚ステータス
+		user.setRole("ROLE_GENERAL");//ロール（一般）
+		
+		//ユーザー登録処理
+		boolean result = userService.insert(user);
+		//ユーザー登録結果の判定
+		if (result==true) {
+			System.out.println("insert成功");
+		} else {
+			System.out.println("insert失敗");
+		}
+
+		return "redirect:/login";
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+--従業員テーブルのデータ（第３章で作成）
+INSERT INTO employee(
+	employee_id
+	,employee_name
+	,age)
+VALUES(
+	1
+	,'山田太郎'
+	,30
+);
+
+--ユーザーマスタのデータ（アドミン権限）
+INSERT INTO m_user(
+	user_id
+	,password
+	,user_name
+	,birthday
+	,age
+	,marriage
+	,role)
+VALUES(
+	'yamada@xxx.co.jp'
+	,'password'
+	,'山田太郎'
+	,'19900101'
+	,28
+	,false
+	,'ROLE_ADMIN'
+);
